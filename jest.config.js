6de9d2eb7517136fc58c4387ccaa22cf112ee856
jibjiby <my01sun@gmail.com>
2023-08-10 19:07:59 +0900

    fix: mjs -> js jest config 파일 모듈 시스템 변경

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
-import nextJest from 'next/jest'
+// import nextJest from 'next/jest'
+const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -8,6 +9,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
+  rootDir: '.',
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
@@ -15,4 +17,5 @@ const config = {
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+// export default createJestConfig(config)
+module.exports = createJestConfig(config)
