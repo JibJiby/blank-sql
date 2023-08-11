c10a6a70b2b011ff6db697c807b415453decca9b
jibjiby <my01sun@gmail.com>
2023-08-11 17:18:28 +0900

    feat: typography-h3 컴포넌트 추가

---
+++ src/components/ui/typography/h3.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react'
+
+import { cn } from '@/lib/utils'
+
+interface Props {
+  children: ReactNode
+  className: string
+}
+
+export default function TypographyH3({ className, children }: Props) {
+  return (
+    <h3 className={cn('text-2xl font-semibold tracking-tight', className)}>
+      {children}
+    </h3>
+  )
+}
