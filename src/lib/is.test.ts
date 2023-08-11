5be8d9154b97eda705182ba2a1f0e9badab83dcf
jibjiby <my01sun@gmail.com>
2023-08-11 15:34:34 +0900

    test: is 파일 테스트코드 추가

---
+++ src/lib/is.test.ts
@@ -0,0 +1,30 @@
+import { isAllNotEmptyInput, isEmptyObject } from './is'
+
+describe('Test Function isAllNotEmptyInput', () => {
+  it('빈 문자열이 아니면 true를 반환한다.', () => {
+    const str = 'SELECT'
+    expect(isAllNotEmptyInput(str)).toBe(true)
+  })
+
+  it('가변 파라미터를 받는다.', () => {
+    expect(isAllNotEmptyInput('a', 'b', 'c')).toBeTruthy()
+  })
+
+  it('빈 문자열이 하나라도 있다면 false를 반환한다.', () => {
+    expect(isAllNotEmptyInput('a', '', 'c')).toBeFalsy()
+  })
+})
+
+describe('Test Function isEmptyObject', () => {
+  it('객체 리터럴은 True를 반환한다.', () => {
+    expect(isEmptyObject({})).toBeTruthy()
+  })
+
+  it('객체 프로퍼티가 하나라도 있다면 false를 반환한다.', () => {
+    const obj = {
+      name: 'object',
+    }
+
+    expect(isEmptyObject(obj)).toBeFalsy()
+  })
+})
