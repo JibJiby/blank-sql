adf51906b68517a4cb840a07cdd5ba24fdbc90ca
jibjiby <my01sun@gmail.com>
2023-08-15 00:18:55 +0900

    style: clipboard copy 버튼 추가

--- src/pages/chapter/[id]/index.tsx
+++ src/pages/chapter/[id]/index.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 
+import { Copy } from 'lucide-react'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -31,6 +33,8 @@ export default function ChapterQuizResolvePage() {
   const [quizId, setQuizId] = useState('')
   const { data } = useSingleQuizQuery(quizId)
 
+  const onClickCopyBtn = () => navigator.clipboard.writeText(quizId)
+
   useEffect(() => {
     setQuizId(router.asPath.split('/').at(-1) || '')
   }, [router])
@@ -39,19 +43,31 @@ export default function ChapterQuizResolvePage() {
 
   return (
     <BaseLayout>
-      <div className="py-8">퀴즈 ID : {quizId}</div>
-      <QuizViewer value={data?.quizQuery} />
-      <div className="flex flex-col items-center mt-8">
-        {/* FIXME: idx 대신 key 값 지정 */}
-        {Array.from({ length: data?.answerLength || 0 }).map((item, idx) => (
-          <div key={idx} className="flex flex-row items-center space-x-4">
-            <Label htmlFor={idx.toString()}>{idx + 1}</Label>
-            <Input id={idx.toString()} />
-          </div>
-        ))}
+      <div className="flex items-center py-8 space-x-6">
+        <span className="select-none">퀴즈 ID : {quizId}</span>
+        <Button
+          variant="outline"
+          size="icon"
+          className="w-[30px] h-[30px]"
+          onClick={onClickCopyBtn}
+        >
+          <Copy width={15} height={15} />
+        </Button>
       </div>
-      <div className="flex justify-center w-full pt-8 max-w-[240px]">
-        <Button className="w-full">제출</Button>
+      <QuizViewer value={data?.quizQuery} />
+      <div className="flex flex-col items-center max-w-[240px]">
+        <div className="flex flex-col items-center mt-8">
+          {/* FIXME: idx 대신 key 값 지정 */}
+          {Array.from({ length: data?.answerLength || 0 }).map((item, idx) => (
+            <div key={idx} className="flex flex-row items-center space-x-4">
+              <Label htmlFor={idx.toString()}>{idx + 1}</Label>
+              <Input id={idx.toString()} />
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-center w-full pt-8 ">
+          <Button className="w-full">제출</Button>
+        </div>
       </div>
     </BaseLayout>
   )
