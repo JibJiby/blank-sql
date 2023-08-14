e72666e79b2c0c16ebc6f37261a41e6745f395c8
jibjiby <my01sun@gmail.com>
2023-08-14 23:10:51 +0900

    refactor: chapter model 분리

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,61 +1,21 @@
 import { rest } from 'msw'
 import { nanoid } from 'nanoid'
-import { z } from 'zod'
 
-// TODO: model 디렉토리로 분리
-// ----
-const ChapterSchema = z.object({
-  chapterId: z.string(),
-  chapterName: z
-    .string()
-    .min(3, '최소 3글자 이상이어야 합니다')
-    .max(40, '최대 40글자 이내로 작성해야 합니다'),
-})
-type Chapter = z.infer<typeof ChapterSchema>
-// ----
+import { Chapter } from '@/models/chapter'
 
 const chapterListMock: Array<Chapter> = [
   { chapterId: nanoid(), chapterName: '기초 개념' },
-  { chapterId: nanoid(), chapterName: '기본 문법 SQL' },
+  { chapterId: nanoid(), chapterName: 'SELECT와 FROM' },
+  { chapterId: nanoid(), chapterName: 'WHERE 으로 원하는 데이터만 가져오기' },
 ]
 
 export const handlers = [
-  rest.get('/api/user', (req, res, ctx) => {
-    return res(
-      // Respond with a 200 status code
-      ctx.status(200)
-    )
-  }),
-
   /**
    * ----------------------------------------------------
    * Chapter
    * ----------------------------------------------------
    */
-
-  // 모든 챕터 목록
   rest.get('/api/chapter', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(chapterListMock))
-  }),
-  // 챕터 생성
-  rest.post('/api/chapter', async (req, res, ctx) => {
-    const { chapterName } = await req.json<Pick<Chapter, 'chapterName'>>()
-
-    const validationResult = ChapterSchema.pick({
-      chapterName: true,
-    }).safeParse({
-      chapterName,
-    })
-    if (!validationResult.success) {
-      return res(ctx.status(400), ctx.text(validationResult.error.message))
-    }
-
-    const newChapter = {
-      chapterId: nanoid(),
-      chapterName,
-    }
-    chapterListMock.push(newChapter)
-
-    return res(ctx.status(201), ctx.json(newChapter))
   }),
 ]
---
+++ src/models/chapter.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+export const ChapterSchema = z.object({
+  chapterId: z.string(),
+  chapterName: z
+    .string()
+    .min(3, '최소 3글자 이상이어야 합니다')
+    .max(40, '최대 40글자 이내로 작성해야 합니다'),
+})
+export type Chapter = z.infer<typeof ChapterSchema>
