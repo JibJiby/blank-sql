c7351fc2f8ee5b60bed0855e3f2b594c3a0e9fbf
jibjiby <my01sun@gmail.com>
2023-08-15 00:06:38 +0900

    feat: quiz-viewer 컴포넌트 추가

---
+++ src/components/quiz-viewer.tsx
@@ -0,0 +1,30 @@
+import AceEditor from 'react-ace'
+
+import 'ace-builds/src-noconflict/mode-mysql'
+import 'ace-builds/src-noconflict/theme-monokai'
+
+import { size } from '@/styles/size'
+
+interface Props {
+  value?: string
+  onChange?: any
+}
+
+function QuizViewer({ value = '', onChange }: Props) {
+  return (
+    <AceEditor
+      mode="mysql"
+      theme="monokai"
+      fontSize={20}
+      showPrintMargin={true}
+      showGutter={true}
+      highlightActiveLine={true}
+      readOnly={true}
+      onChange={onChange}
+      value={value}
+      style={{ height: size.quizViewerHeight }}
+    />
+  )
+}
+
+export default QuizViewer
