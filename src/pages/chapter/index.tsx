7dbbde501fa7f7ad030ca9f5f41617128fd1fb9b
jibjiby <my01sun@gmail.com>
2023-08-14 23:21:00 +0900

    feat: chapter data 렌더링 처리

--- src/pages/chapter/index.tsx
+++ src/pages/chapter/index.tsx
@@ -1,15 +1,37 @@
-import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import { useChapterQuery } from '@/hooks/query/useChapterQuery'
 
 import BaseLayout from '@/layouts/base-layout'
+import { Chapter } from '@/models/chapter'
 
 export default function ChapterQuizPage() {
-  // TODO: server state로 빼기
-  useEffect(() => {
-    ;(async function () {
-      const res = await fetch('/api/chapter')
-      console.log(await res.json())
-    })()
-  }, [])
+  const router = useRouter()
+  const { data } = useChapterQuery()
 
-  return <BaseLayout>chapter quiz page</BaseLayout>
+  const onClickChapter = (chapterId: string) => {
+    return () => {
+      router.push(router.asPath + `/${chapterId}`)
+    }
+  }
+
+  const renderChapterList = (chapterList: Chapter[]) => {
+    return data.map((chapter) => (
+      <div
+        onClick={onClickChapter(chapter.chapterId)}
+        key={`chapter-${chapter.chapterId}`}
+        className="w-full px-4 py-6 font-semibold text-center transition-all ease-in border rounded-md cursor-pointer hover:bg-secondary"
+      >
+        {chapter.chapterName}
+      </div>
+    ))
+  }
+
+  return (
+    <BaseLayout>
+      <div className="flex flex-col items-center space-y-8">
+        {renderChapterList(data)}
+      </div>
+    </BaseLayout>
+  )
 }
