2227f9c845878f43e98e0d392ed6b71a5ff7fde6
jibjiby <my01sun@gmail.com>
2023-08-15 00:07:54 +0900

    test: single quiz api 에 대한 mock handler 작성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,11 +2,22 @@ import { rest } from 'msw'
 import { nanoid } from 'nanoid'
 
 import { Chapter } from '@/models/chapter'
+import { Quiz } from '@/models/quiz'
 
 const chapterListMock: Array<Chapter> = [
   { chapterId: nanoid(), chapterName: '기초 개념' },
   { chapterId: nanoid(), chapterName: 'SELECT와 FROM' },
   { chapterId: nanoid(), chapterName: 'WHERE 으로 원하는 데이터만 가져오기' },
+]
+
+const quizListMock: Array<Quiz> = [
+  {
+    quizId: nanoid(),
+    quizQuery: 'SELECT\n\t____\nFROM\n\tMY_DB.USERS',
+    answerObj: '{"0":"*"}',
+    answerLength: 1,
+    priority: 50000,
+  },
 ]
 
 export const handlers = [
@@ -17,5 +28,14 @@ export const handlers = [
    */
   rest.get('/api/chapter', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(chapterListMock))
+  }),
+  /**
+   * ----------------------------------------------------
+   * Quiz
+   * ----------------------------------------------------
+   */
+  rest.get('/api/quiz/:id', (req, res, ctx) => {
+    // const { id } = req.params
+    return res(ctx.status(200), ctx.json(quizListMock[0]))
   }),
 ]
