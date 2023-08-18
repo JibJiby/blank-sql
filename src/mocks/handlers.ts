9696e593afbbdf67ee1af01dacea2a9bf19128fc
jibjiby <my01sun@gmail.com>
2023-08-18 16:16:36 +0900

    feat: mock api quiz 전체 목록 조회 추가 및 id 단순화

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -5,17 +5,24 @@ import { Chapter } from '@/models/chapter'
 import { Quiz } from '@/models/quiz'
 
 const chapterListMock: Array<Chapter> = [
-  { chapterId: nanoid(), chapterName: '기초 개념' },
-  { chapterId: nanoid(), chapterName: 'SELECT와 FROM' },
-  { chapterId: nanoid(), chapterName: 'WHERE 으로 원하는 데이터만 가져오기' },
+  { chapterId: '1', chapterName: '기초 개념' },
+  { chapterId: '2', chapterName: 'SELECT와 FROM' },
+  { chapterId: '3', chapterName: 'WHERE 으로 원하는 데이터만 가져오기' },
 ]
 
 const quizListMock: Array<Quiz> = [
   {
-    quizId: nanoid(),
+    quizId: '1',
     quizQuery: 'SELECT\n\t____\nFROM\n\tMY_DB.USERS',
     answerObj: '{"0":"*"}',
     answerLength: 1,
+    priority: 50000,
+  },
+  {
+    quizId: '2',
+    quizQuery: 'SELECT\n\t____\nFROM\n\tMY_DB.____',
+    answerObj: '{"0":"*", "1", "USERS"}',
+    answerLength: 2,
     priority: 50000,
   },
 ]
@@ -34,8 +41,19 @@ export const handlers = [
    * Quiz
    * ----------------------------------------------------
    */
+  rest.get('/api/quiz', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(quizListMock))
+  }),
+
   rest.get('/api/quiz/:id', (req, res, ctx) => {
-    // const { id } = req.params
-    return res(ctx.status(200), ctx.json(quizListMock[0]))
+    const { id } = req.params
+
+    const findQuizIdx = quizListMock.findIndex((quiz) => quiz.quizId === id)
+    if (findQuizIdx === -1) {
+      return res(ctx.status(403))
+    }
+
+    const quiz = quizListMock.at(findQuizIdx)
+    return res(ctx.status(200), ctx.json(quiz))
   }),
 ]
