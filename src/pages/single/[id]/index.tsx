1e1eb948beb3592ea19a81310d90afc2da4f7d2c
jibjiby <my01sun@gmail.com>
2023-08-20 23:23:30 +0900

    feat: single quiz resolve page 정답 체크 로직 추가

--- src/pages/single/[id]/index.tsx
+++ src/pages/single/[id]/index.tsx
@@ -1,11 +1,19 @@
-import dynamic from 'next/dynamic'
+import { useRef } from 'react'
+
 import { useRouter } from 'next/router'
 
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { useToast } from '@/components/ui/use-toast'
 
+import {
+  QUIZ_SINGLE_INPUT_PREFIX,
+  QuizSingleInput,
+} from '@/components/quiz-single-input'
 import QuizViewer from '@/components/quiz-viewer'
+
+import { isAllNotEmptyInput } from '@/lib/is'
+import { matchAnswer } from '@/lib/match-answer'
+import { makePrefixKey, range } from '@/lib/utils'
 
 import { useSingleQuizQuery } from '@/hooks/query/useSingleQuizQuery'
 
@@ -14,35 +22,88 @@ import BaseLayout from '@/layouts/base-layout'
 export default function SingleQuizResolverPage() {
   const router = useRouter()
   const { data } = useSingleQuizQuery(router.asPath.at(-1) || '')
+  const inputMapRef = useRef<Map<string, string>>(new Map())
+  const { toast } = useToast()
 
-  // const onClick = () => {
-  //   const quizAnswer = targetQuizData?.answerObj ?? null
-  //   if (quizAnswer === null) return
-  //   const quizAnswerObj = JSON.parse(quizAnswer)
+  const onSubmit = async () => {
+    if (!data || !data?.answerLength || isNaN(Number(data?.answerLength))) {
+      return
+    }
 
-  //   if (matchAnswer(answerArray, quizAnswerObj)) {
-  // }
+    for (const idx of range(Number(data.answerLength))) {
+      const inputText = findInputValueByLabelNumber(idx)
+
+      if (!inputText) {
+        return toast({
+          title: '빠짐없이 입력해주세요',
+          variant: 'destructive',
+          duration: 1500,
+        })
+      }
+
+      inputMapRef.current.set(idx.toString(), inputText)
+    }
+
+    if (!isAllNotEmptyInput(...Array.from(inputMapRef.current.values()))) {
+      return
+    }
+
+    const quizAnswer = data.answerObj ?? null
+    if (quizAnswer === null) return
+
+    const answer = JSON.parse(quizAnswer)
+    if (!matchAnswer(Object.fromEntries(inputMapRef.current), answer)) {
+      toast({
+        title: '틀렸습니다',
+        variant: 'destructive',
+      })
+      return
+    }
+
+    await router.push('/')
+    toast({
+      title: '정답 입니다!',
+      className: 'bg-emerald-500',
+      duration: 1000,
+    })
+  }
 
   return (
     <BaseLayout>
       <QuizViewer value={data?.quizQuery} />
       <div className="flex flex-col items-center max-w-[240px]">
         <div className="flex flex-col items-center mt-8">
-          {Array.from({ length: data?.answerLength || 0 }).map((_, idx) => (
-            <div key={idx} className="flex flex-row items-center mt-4">
-              <Label htmlFor={idx.toString()} className="p-2 select-none">
-                {idx + 1}
-              </Label>
-              <Input id={idx.toString()} className="ml-4" />
-            </div>
-          ))}
+          {data &&
+            Array.from({ length: data.answerLength }).map((_, idx) => (
+              <QuizSingleInput
+                key={makePrefixKey({
+                  prefix: QUIZ_SINGLE_INPUT_PREFIX,
+                  keyValue: (idx + 1).toString(),
+                })}
+                label={(idx + 1).toString()}
+              />
+            ))}
         </div>
         <div className="flex justify-center w-full pt-8 ">
-          <Button className="w-full" onClick={() => {}}>
+          <Button className="w-full" onClick={onSubmit}>
             제출
           </Button>
         </div>
       </div>
     </BaseLayout>
   )
+}
+
+function findInputValueByLabelNumber(labelNumber: number): string {
+  const inputText =
+    (
+      document.getElementById(
+        makePrefixKey({
+          prefix: QUIZ_SINGLE_INPUT_PREFIX,
+          keyValue: (labelNumber + 1).toString(),
+        })
+      ) as HTMLInputElement
+    )?.value || ''
+
+  return inputText
 }
