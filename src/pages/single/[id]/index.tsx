06329866236aa7e83989ae869b0ec467b059e6a8
jibjiby <my01sun@gmail.com>
2023-08-20 19:55:35 +0900

    refactor: 변경된 quiz-view 컴포넌트 임포트 처리

--- src/pages/single/[id]/index.tsx
+++ src/pages/single/[id]/index.tsx
@@ -4,29 +4,24 @@ import { useRouter } from 'next/router'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Skeleton } from '@/components/ui/skeleton'
+
+import QuizViewer from '@/components/quiz-viewer'
 
 import { useSingleQuizQuery } from '@/hooks/query/useSingleQuizQuery'
 
-import { size } from '@/styles/size'
-
 import BaseLayout from '@/layouts/base-layout'
-
-const QuizViewer = dynamic(import('@/components/quiz-viewer'), {
-  ssr: false,
-  loading: () => (
-    <Skeleton
-      style={{
-        height: size.quizViewerHeight,
-        width: size.quizViewerWidth,
-      }}
-    />
-  ),
-})
 
 export default function SingleQuizResolverPage() {
   const router = useRouter()
   const { data } = useSingleQuizQuery(router.asPath.at(-1) || '')
+
+  // const onClick = () => {
+  //   const quizAnswer = targetQuizData?.answerObj ?? null
+  //   if (quizAnswer === null) return
+  //   const quizAnswerObj = JSON.parse(quizAnswer)
+
+  //   if (matchAnswer(answerArray, quizAnswerObj)) {
+  // }
 
   return (
     <BaseLayout>
