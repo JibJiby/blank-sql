3f39ea34ab4a0e3422343953ecb6d641a97d802c
jibjiby <my01sun@gmail.com>
2023-08-20 23:22:36 +0900

    feat: makePrefixKey util function 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * prefix 와 keyValue를 조합하여 반환
+ */
+export function makePrefixKey({
+  prefix,
+  keyValue,
+}: {
+  prefix: string
+  keyValue: string
+}) {
+  return `${prefix}_${keyValue}`
+}
+
+export function range(start: number, end?: number) {
+  if (!end) {
+    return Array.from({ length: start }, (_, idx) => idx)
+  }
+
+  return Array.from({ length: end - start }, (_, idx) => start + idx - 1)
+}
