e1fd08cd58d17e0e3f50e1796e5d3d9b2227f4b4
jibjiby <my01sun@gmail.com>
2023-08-22 12:39:02 +0900

    refactor: time constants 분리

---
+++ src/hooks/query/constants.ts
@@ -0,0 +1,14 @@
+const millisecond = 1
+const second = millisecond * 1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24
+const week = day * 7
+
+export const time = {
+  SECOND: second,
+  MINUTE: minute,
+  HOUR: hour,
+  DAY: day,
+  WEEK: week,
+}
