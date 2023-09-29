de53a901e97e4a206726f1eeea32c58837fd3eed
jibjiby <my01sun@gmail.com>
2023-09-29 15:04:52 +0900

    chore: middleware matcher 변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,11 @@ import { authMiddleware } from '@clerk/nextjs'
 export default authMiddleware({})
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!.+\\.[\\w]+$|_next).*)',
+    '/',
+    // '/(api|trpc)(.*)',
+    // // https://nextjs.org/docs/messages/invalid-route-source
+    '/api/((?!webhook).*)',
+  ],
 }
