f32ce17312c74aa62c9640828be0c6b4f175145c
jibjiby <my01sun@gmail.com>
2023-10-02 19:05:58 +0900

    chore: middlware api 접근 재수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,5 @@ import { authMiddleware } from '@clerk/nextjs'
 export default authMiddleware({})
 
 export const config = {
-  matcher: [
-    '/((?!.+\\.[\\w]+$|_next).*)',
-    '/',
-    // '/(api|trpc)(.*)',
-    // // https://nextjs.org/docs/messages/invalid-route-source
-    '/api/((?!webhook).*)',
-  ],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
