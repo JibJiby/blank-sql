263fcf111ac4e79a24b569e99491d4916ecf551e
jibjiby <my01sun@gmail.com>
2023-10-12 17:53:01 +0900

    feat: auth 관련 로직 추가

--- src/components/user-auth-form.tsx
+++ src/components/user-auth-form.tsx
@@ -22,7 +22,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
         className={cn(buttonVariants({ variant: 'outline' }))}
         onClick={() => {
           setIsGoogleLoading(true)
-          signIn('github')
+          signIn('google')
         }}
         disabled={isLoading || isGoogleLoading}
       >
