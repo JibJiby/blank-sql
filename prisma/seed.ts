6b0e50ec0ba07a731e36276aa94b8a30cd660d25
jibjiby <my01sun@gmail.com>
2023-10-16 13:53:13 +0900

    refactor(prisma): db schema 변경

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,38 +4,10 @@ const prisma = new PrismaClient()
 
 // TODO: 원하시는 seeding 처리로 변경하시면 됩니다.
 async function main() {
-  // const firstUser = await prisma.user.findFirst({
-  //   where: {
-  //     email: {
-  //       startsWith: 'newblanksql',
-  //     },
-  //   },
-  // })
-
-  // if (firstUser) {
-  //   const randomId = Math.random().toString(36).substring(2, 11)
-  //   const user = await prisma.user.create({
-  //     data: {
-  //       email: `${randomId}@newblanksql.io`,
-  //     },
-  //   })
-  //   console.log(user)
-  // } else {
-  //   const user = await prisma.user.create({
-  //     data: {
-  //       email: 'newblanksql@newblanksql.io',
-  //     },
-  //   })
-  //   console.log(user)
-  // }
-
-  // --------------------------------------------------------------
-
   const newChapterID = Math.random().toString(36).substring(2, 11)
 
   const chapter = await prisma.chapter.create({
     data: {
-      chapterId: newChapterID,
       chapterName: '기초',
     },
   })
@@ -44,9 +16,9 @@ async function main() {
 
   const quiz = await prisma.quiz.create({
     data: {
-      quizId: '1',
       quizQuery: 'SELECT\n\t____\nFROM\n\tMY_DB.USERS',
       answerObj: JSON.stringify({ 0: '*' }),
+      // TODO: 추후 제거
       answerLength: 1,
       chapterId: newChapterID,
     },
