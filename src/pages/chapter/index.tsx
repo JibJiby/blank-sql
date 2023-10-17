349bef5806eac2f7f726fb9ad9f7334dd7226900
jibjiby <my01sun@gmail.com>
2023-10-17 23:57:13 +0900

    refactor: pre-render를 위한 router_push --> Link 로 변경

--- src/pages/chapter/index.tsx
+++ src/pages/chapter/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 import { container } from 'tsyringe'
@@ -16,25 +17,19 @@ export default function ChapterQuizPage({
   const router = useRouter()
   const chapters = JSON.parse(data)
 
-  const handleChapterIdInput = (chapterId: string) => {
-    return () => {
-      router.push(router.asPath + `/${chapterId}`)
-    }
-  }
-
   const renderChapterList = (chapterList: Chapter[]) => {
     if (!chapterList.length) {
       return
     }
 
     return chapterList.map((chapter) => (
-      <div
-        onClick={handleChapterIdInput(chapter.id)}
+      <Link
         key={`chapter-${chapter.id}`}
+        href={router.asPath + `/${chapter.id}`}
         className="w-full px-4 py-6 font-semibold text-center transition-all ease-in border rounded-md cursor-pointer hover:bg-secondary"
       >
         {chapter.chapterName}
-      </div>
+      </Link>
     ))
   }
 
