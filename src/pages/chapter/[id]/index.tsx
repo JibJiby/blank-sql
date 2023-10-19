b96783ece2804ef2a200b807ec58a58e9e9a1cf5
jibjiby <my01sun@gmail.com>
2023-10-19 16:32:33 +0900

    refactor: quiz-input-form 적용

--- src/pages/chapter/[id]/index.tsx
+++ src/pages/chapter/[id]/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { useRouter } from 'next/router'
 
@@ -13,15 +13,8 @@ import {
 } from '@/components/ui/tooltip'
 import { useToast } from '@/components/ui/use-toast'
 
-import {
-  QUIZ_SINGLE_INPUT_PREFIX,
-  QuizSingleInput,
-} from '@/components/quiz-single-input'
+import { QuizInputForm } from '@/components/quiz-input-form'
 import QuizViewer from '@/components/quiz-viewer'
-
-import { isAllNotEmptyInput } from '@/lib/is'
-import { matchAnswer } from '@/lib/match-answer'
-import { makePrefixKey, range } from '@/lib/utils'
 
 import { useQuizInChapterQuery } from '@/hooks/query/use-quiz-in-chapter-query'
 
@@ -33,62 +26,22 @@ export default function ChapterQuizResolverPage() {
   const [chapterId, setChapterId] = useState('')
   const { toast } = useToast()
   const [sequence, setSequence] = useState(0)
-  const {
-    data: quizzesInChapter,
-    status,
-    error,
-  } = useQuizInChapterQuery(chapterId)
-  const answerLength =
-    quizzesInChapter?.length && status === 'success' && !error
-      ? Object.keys(JSON.parse(quizzesInChapter[sequence].answer || '')).length
-      : 0
-  const inputMapRef = useRef<Map<string, string>>(new Map())
+  const { data: quizzesInChapter, status } = useQuizInChapterQuery(chapterId)
 
-  const onClickCopyBtn = () => {
-    if (quizzesInChapter && answerLength) {
-      navigator.clipboard.writeText(quizzesInChapter[sequence].id)
+  const quizzesInChapterLength = quizzesInChapter ? quizzesInChapter.length : 0
+  const quiz =
+    quizzesInChapter && quizzesInChapter.at(sequence)
+      ? quizzesInChapter[sequence]
+      : null
+
+  const handleCopyButton = () => {
+    if (quiz) {
+      navigator.clipboard.writeText(quiz.id)
     }
   }
 
-  const onClickNext = async () => {
-    if (!quizzesInChapter?.length) {
-      return
-    }
-
-    for (const idx of range(answerLength)) {
-      const inputText = findInputValueByLabelNumber(idx)
-
-      if (!inputText) {
-        return toast({
-          title: '빠짐없이 입력해주세요',
-          variant: 'destructive',
-          duration: 1500,
-        })
-      }
-
-      inputMapRef.current.set(idx.toString(), inputText)
-    }
-
-    if (!isAllNotEmptyInput(...Array.from(inputMapRef.current.values()))) {
-      return
-    }
-
-    const quizAnswer = quizzesInChapter.at(sequence)?.answer ?? null
-    if (quizAnswer === null) return
-
-    const answer = JSON.parse(quizAnswer)
-    if (!matchAnswer(Object.fromEntries(inputMapRef.current), answer)) {
-      toast({
-        title: '틀렸습니다',
-        variant: 'destructive',
-      })
-      return
-    }
-
-    // 정답인 경우 reset
-    inputMapRef.current = new Map()
-
-    if (sequence + 1 === quizzesInChapter.length) {
+  const handleSuccess = async () => {
+    if (sequence + 1 === quizzesInChapterLength) {
       await router.push('/')
       toast({
         title: '전부 맞추셨습니다! 축하드립니다~',
@@ -106,6 +59,14 @@ export default function ChapterQuizResolverPage() {
     })
   }
 
+  const handleFailure = async () => {
+    toast({
+      title: '틀렸습니다',
+      variant: 'destructive',
+    })
+    return
+  }
+
   useEffect(() => {
     if (!id) {
       return
@@ -117,7 +78,7 @@ export default function ChapterQuizResolverPage() {
     return
   }
 
-  if (!quizzesInChapter.length) {
+  if (!quizzesInChapter || !quizzesInChapter.length) {
     return
   }
 
@@ -127,61 +88,32 @@ export default function ChapterQuizResolverPage() {
         <span className="select-none">
           퀴즈 ID : {quizzesInChapter.at(sequence)?.id || ''}
         </span>
-        <CopyButton handler={onClickCopyBtn} />
+        <CopyButton onCopy={handleCopyButton} />
       </div>
       <QuizViewer value={quizzesInChapter.at(sequence)?.quiz} />
-      <div className="flex flex-col items-center max-w-[240px]">
-        <div className="flex flex-col items-center mt-8">
-          {Array.from({
-            length: answerLength || 0,
-          }).map((_, idx) => (
-            <QuizSingleInput
-              key={makePrefixKey({
-                prefix: `${QUIZ_SINGLE_INPUT_PREFIX}_${chapterId}_${sequence}`,
-                keyValue: (idx + 1).toString(),
-              })}
-              label={(idx + 1).toString()}
-            />
-          ))}
-        </div>
-        <div className="flex justify-center w-full pt-8 ">
-          <Button className="w-full" onClick={onClickNext}>
-            제출
-          </Button>
-        </div>
-      </div>
+      <QuizInputForm
+        quiz={quizzesInChapter[sequence]}
+        onSuccess={handleSuccess}
+        onFailure={handleFailure}
+      />
     </BaseLayout>
   )
 }
 
-function findInputValueByLabelNumber(labelNumber: number): string {
-  const inputText =
-    (
-      document.getElementById(
-        makePrefixKey({
-          prefix: QUIZ_SINGLE_INPUT_PREFIX,
-          keyValue: (labelNumber + 1).toString(),
-        })
-      ) as HTMLInputElement
-    )?.value || ''
-
-  return inputText
+type CopyButtonProps = {
+  onCopy: () => void
 }
 
-function CopyButton({ handler }: { handler: () => void }) {
+function CopyButton({ onCopy }: CopyButtonProps) {
   return (
-    <TooltipProvider
-      delayDuration={0}
-      skipDelayDuration={300}
-      disableHoverableContent
-    >
+    <TooltipProvider delayDuration={0} skipDelayDuration={300}>
       <Tooltip>
         <TooltipTrigger asChild>
           <Button
             variant="outline"
             size="icon"
             className="w-[30px] h-[30px]"
-            onClick={handler}
+            onClick={onCopy}
           >
             <Copy width={15} height={15} />
           </Button>
