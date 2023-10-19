6680dae827a7a7ffaef0ed9a8c6bc90f28beb193
jibjiby <my01sun@gmail.com>
2023-10-20 00:37:27 +0900

    chore: query default option 변경

--- src/lib/query.ts
+++ src/lib/query.ts
@@ -3,8 +3,8 @@ import { QueryClient } from '@tanstack/react-query'
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchIntervalInBackground: false,
       refetchOnWindowFocus: false,
+      refetchOnMount: false,
     },
   },
 })
