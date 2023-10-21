06d906b0dbfc2d413dd9c7e2f43c15714b241deb
jibjiby <my01sun@gmail.com>
2023-10-21 10:23:25 +0900

    test: jest-dom 을 위한 jest.setup.js 설정 파일 추가

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ const createJestConfig = nextJest({
 const config = {
   rootDir: '.',
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
   testEnvironment: 'jest-environment-jsdom',
 
