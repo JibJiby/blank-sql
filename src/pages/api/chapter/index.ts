806220543a24f767286d99712c9c5204cb06e39b
jibjiby <my01sun@gmail.com>
2023-10-25 15:57:53 +0900

    refactor: chapter 생성/수정/삭제 admin api 로 이동

--- src/pages/api/chapter/index.ts
+++ src/pages/api/chapter/index.ts
@@ -9,29 +9,6 @@ export default async function handler(
   if (req.method === 'GET') {
     const chapters = await chapterService.getAllChapters()
     return res.status(200).json(chapters)
-  } else if (req.method === 'POST') {
-    // TODO: role 체크
-    // TODO: API Request, Response 데이터 타입 분리
-    const { newChapterName } = req.body
-    if (!newChapterName) {
-      return res.status(400).send(null)
-    }
-
-    const newChapter = await chapterService.createChapter(newChapterName)
-
-    return res.status(201).json(newChapter)
-  } else if (req.method === 'DELETE') {
-    const chapterId = req.body
-    const deletedChapter = await chapterService.deleteChapter(chapterId)
-    return res.status(200).json(deletedChapter)
-  } else if (req.method === 'PATCH') {
-    const { chapterId, newChapterName } = req.body
-    const updatedChapter = await chapterService.updateChapter(
-      chapterId,
-      newChapterName
-    )
-
-    return res.status(200).json(updatedChapter)
   }
 
   return res.status(400)
