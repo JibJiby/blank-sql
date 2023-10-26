0e2094a5ba5749d40c71d2d5ee27c6ad91189141
jibjiby <my01sun@gmail.com>
2023-10-27 01:34:29 +0900

    feat: admin quiz 수정/삭제 기능 추가

--- src/pages/api/admin/quiz/index.ts
+++ src/pages/api/admin/quiz/index.ts
@@ -39,18 +39,20 @@ export default async function handler(
 
     const newQuiz = await quizService.createQuiz(chapterId, quiz, answer)
     return res.status(201).json(newQuiz)
-    // } else if (req.method === 'DELETE') {
-    //   const chapterId = req.body
-    //   const deletedChapter = await chapterService.deleteChapter(chapterId)
-    //   return res.status(200).json(deletedChapter)
-    // } else if (req.method === 'PATCH') {
-    //   const { chapterId, newChapterName } = req.body
-    //   const updatedChapter = await chapterService.updateChapter(
-    //     chapterId,
-    //     newChapterName
-    //   )
+  } else if (req.method === 'DELETE') {
+    const quizId = req.body
+    const deletedQuiz = await quizService.deleteQuiz(quizId)
+    return res.status(200).json(deletedQuiz)
+  } else if (req.method === 'PATCH') {
+    const { quizId, quiz, answer, chapterId } = req.body
+    const updatedQuiz = await quizService.updateQuiz(
+      quizId,
+      quiz,
+      answer,
+      chapterId
+    )
 
-    //   return res.status(200).json(updatedChapter)
+    return res.status(200).json(updatedQuiz)
   }
 
   return res.status(400)
