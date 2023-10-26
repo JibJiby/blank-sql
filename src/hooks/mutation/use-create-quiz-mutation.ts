79245b7e68b296f7e9a163aa4a34713803b357ee
jibjiby <my01sun@gmail.com>
2023-10-27 00:56:00 +0900

    fixed: invalidate-quries 대상 수정

--- src/hooks/mutation/use-create-quiz-mutation.ts
+++ src/hooks/mutation/use-create-quiz-mutation.ts
@@ -25,7 +25,7 @@ export const useCreateQuizMutation = () => {
       }
     },
     onSuccess: () => {
-      queryClient.invalidateQueries(['quizzes'])
+      queryClient.invalidateQueries(['paging', 'quizzes'])
     },
   })
 
