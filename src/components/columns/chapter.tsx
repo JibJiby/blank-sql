02bc651243fd674b9945b4dcad7fe9b5a49f67d9
jibjiby <my01sun@gmail.com>
2023-10-27 09:48:57 +0900

    refactor: chapter columns 내부 컴포넌트 분리

--- src/components/columns/chapter.tsx
+++ src/components/columns/chapter.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 
-import { ColumnDef } from '@tanstack/react-table'
+import { CellContext, ColumnDef } from '@tanstack/react-table'
 import { Pencil, Trash2 } from 'lucide-react'
 
 import {
@@ -44,86 +44,105 @@ export function getColumns({ editHandler, deleteHandler }: Params) {
      */
     {
       id: 'editAction',
-      cell: ({ row }) => {
-        // TODO: cell render component 따로 분리할지 고려
-        const chapterId = row.original.id
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const inputRef = useRef<HTMLInputElement | null>(null)
-        // TODO: validation 을 caller 에서 받는것 고려
-        const handler = () => {
-          if (!inputRef.current) {
-            return
-          }
-          const newChapterInput = inputRef.current.value.trim()
-          try {
-            editHandler(chapterId, newChapterInput)
-            inputRef.current.value = ''
-          } catch (err) {
-            console.error(err)
-          }
-        }
-
-        return (
-          <div className="flex items-center justify-center">
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button variant="ghost" size="sm">
-                  <Pencil width={16} />
-                </Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent className="max-sm:w-[80%]">
-                <AlertDialogHeader>
-                  <AlertDialogTitle>
-                    챕터명을 수정하시겠습니까?
-                  </AlertDialogTitle>
-                </AlertDialogHeader>
-                <div>
-                  <Input ref={inputRef} />
-                </div>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>취소</AlertDialogCancel>
-                  <AlertDialogAction onClick={handler}>수정</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </div>
-        )
-      },
+      cell: (props) => (
+        <ChapterEditButtonCell {...props} editHandler={editHandler} />
+      ),
     },
     /**
      * 삭제
      */
     {
       id: 'deleteAction',
-      cell: ({ row }) => {
-        const chapterId = row.original.id
-        const handler = () => deleteHandler(chapterId)
-
-        return (
-          <div className="flex items-center justify-center">
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button variant="ghost" size="sm">
-                  <Trash2 width={16} />
-                </Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent className="max-sm:w-[80%]">
-                <AlertDialogHeader>
-                  <AlertDialogTitle>
-                    해당 챕터를 삭제하시겠습니까?
-                  </AlertDialogTitle>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>취소</AlertDialogCancel>
-                  <AlertDialogAction onClick={handler}>삭제</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </div>
-        )
-      },
+      cell: (props) => (
+        <ChapterDeleteButtonCell {...props} deleteHandler={deleteHandler} />
+      ),
     },
   ]
 
   return columns
+}
+
+/**
+ * ------------------------------------------------------
+ * 내부 컴포넌트
+ * ------------------------------------------------------
+ */
+
+type ChapterEditButtonCellProps = CellContext<Chapter, unknown> &
+  Pick<Params, 'editHandler'>
+
+function ChapterEditButtonCell({
+  row,
+  editHandler,
+}: ChapterEditButtonCellProps) {
+  const chapterId = row.original.id
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const handler = () => {
+    if (!inputRef.current) {
+      return
+    }
+    const newChapterInput = inputRef.current.value.trim()
+    try {
+      editHandler(chapterId, newChapterInput)
+      inputRef.current.value = ''
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return (
+    <div className="flex items-center justify-center">
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="ghost" size="sm">
+            <Pencil width={16} />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent className="max-sm:w-[80%]">
+          <AlertDialogHeader>
+            <AlertDialogTitle>챕터명을 수정하시겠습니까?</AlertDialogTitle>
+          </AlertDialogHeader>
+          <div>
+            <Input ref={inputRef} />
+          </div>
+          <AlertDialogFooter>
+            <AlertDialogCancel>취소</AlertDialogCancel>
+            <AlertDialogAction onClick={handler}>수정</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  )
+}
+
+type ChapterDeleteButtonCellProps = CellContext<Chapter, unknown> &
+  Pick<Params, 'deleteHandler'>
+
+function ChapterDeleteButtonCell({
+  row,
+  deleteHandler,
+}: ChapterDeleteButtonCellProps) {
+  const chapterId = row.original.id
+  const handler = () => deleteHandler(chapterId)
+
+  return (
+    <div className="flex items-center justify-center">
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="ghost" size="sm">
+            <Trash2 width={16} />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent className="max-sm:w-[80%]">
+          <AlertDialogHeader>
+            <AlertDialogTitle>해당 챕터를 삭제하시겠습니까?</AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>취소</AlertDialogCancel>
+            <AlertDialogAction onClick={handler}>삭제</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  )
 }
