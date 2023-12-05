616380328fba27f99e92ad68e11fb096944a7f66
jibjiby <my01sun@gmail.com>
2023-12-06 02:06:00 +0900

    feat: 반응형 메뉴 네비게이션 추가

--- src/components/common/page-header.tsx
+++ src/components/common/page-header.tsx
@@ -1,13 +1,14 @@
-import { CSSProperties, useEffect, useState } from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+import { CSSProperties, PropsWithChildren, useEffect, useState } from 'react'
 
 import Link from 'next/link'
 
-import { Moon, Sun } from 'lucide-react'
+import { AlignJustify, Moon, Sun } from 'lucide-react'
 import { signOut, useSession } from 'next-auth/react'
 import { useTheme } from 'next-themes'
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -16,6 +17,16 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+
+import { cn } from '@/lib/utils'
 
 import { size } from '@/styles/size'
 import { zIdx } from '@/styles/z-index'
@@ -24,25 +35,60 @@ const headerStyle: CSSProperties = {
   height: size.headerHeight,
 }
 
+const menuList = [
+  { href: '/single', text: '하나씩 풀기' },
+  { href: '/chapter', text: '챕터별 풀기' },
+]
+
 export function PageHeader() {
   return (
     <header
       style={headerStyle}
-      className={`fixed z-${zIdx.header} top-0 w-full  flex justify-between py-4 px-8`}
+      className={`fixed z-${zIdx.header} top-0 w-full  flex justify-between py-4 pl-8 pr-4`}
     >
-      <div className="flex items-center justify-between w-full h-full font-bold">
+      <div className="flex items-center w-full h-full font-bold gap-x-4">
         <Link href="/">_____SQL</Link>
+        <nav className="flex justify-center ml-4 font-normal max-sm:hidden gap-x-6">
+          {menuList.map((menu) => (
+            <PageHeader.NavItem
+              key={menu.href}
+              href={menu.href}
+              text={menu.text}
+            />
+          ))}
+        </nav>
       </div>
-      <div className="flex space-x-4">
-        <ThemeSwitch />
-        <UserNav />
+      <div className="flex gap-x-4">
+        <PageHeader.ThemeSwitch />
+        <PageHeader.UserNav />
+        <PageHeader.Menu className="sm:hidden" />
       </div>
     </header>
   )
 }
 
+function _HeaderNavItem({
+  href,
+  text,
+  className,
+}: PropsWithChildren<{ href: string; text: string; className?: string }>) {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        buttonVariants({ variant: 'ghost' }),
+        'opacity-60 hover:opacity-100 m-0',
+        className
+      )}
+    >
+      {text}
+    </Link>
+  )
+}
+PageHeader.NavItem = _HeaderNavItem
+
 // ref : https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
-function ThemeSwitch() {
+function _ThemeSwitch() {
   const [mounted, setMounted] = useState(false)
   const { setTheme } = useTheme()
 
@@ -57,7 +103,7 @@ function ThemeSwitch() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger asChild className="max-sm:hidden">
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
@@ -77,8 +123,9 @@ function ThemeSwitch() {
     </DropdownMenu>
   )
 }
+PageHeader.ThemeSwitch = _ThemeSwitch
 
-function UserNav() {
+function _UserNav() {
   const [noImg, setNoImg] = useState(false)
   const { data } = useSession()
 
@@ -125,3 +172,43 @@ function UserNav() {
     </DropdownMenu>
   )
 }
+PageHeader.UserNav = _UserNav
+
+function _Menu({ className }: { className?: string }) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <div
+          className={cn(
+            'flex items-center justify-center cursor-pointer w-10 h-10',
+            className
+          )}
+        >
+          <Button
+            variant="ghost"
+            size="icon"
+            className="opacity-75 hover:opacity-100"
+          >
+            <AlignJustify strokeWidth={2.4} size={20} />
+          </Button>
+        </div>
+      </SheetTrigger>
+      <SheetContent className="w-[45%]">
+        <SheetHeader>
+          <SheetTitle>메뉴</SheetTitle>
+          <SheetDescription>
+            {menuList.map((menu) => (
+              <PageHeader.NavItem
+                className="w-full"
+                key={menu.href}
+                href={menu.href}
+                text={menu.text}
+              />
+            ))}
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  )
+}
+PageHeader.Menu = _Menu
